055a5e55b24e0a2eba9ea892ba2f36abc1912603
Shelby Butt <shelbybutt@Shelbys-MacBook-Pro.local>
2021-09-08 20:31:50 -0400

    working on controller

---
+++ reviews/controller/dashboard-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+//import models
+//import authentication fucntion
+
+
+
+
+//route to get all posts (check to see if a user is loggedIn )
+
+//route to render form to create a new post (should only respond with handlebars file)
+
+// get route to edit a specific post
+
+
+module.exports =router;
