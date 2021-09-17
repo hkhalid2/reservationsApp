530f365e814d425c581d4dda79f052e24a843e37
Hamza Khalid <hamzer95@gmail.com>
2021-09-17 18:12:58 -0400

    styling changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,11 +28,6 @@ router.get('/reservations', async (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  // // If the user is already logged in, redirect the request to another route
-  // if (req.session.logged_in) {
-  //   res.redirect('/profile');
-  //   return;
-  // }
   
  res.render('menu');
 });
